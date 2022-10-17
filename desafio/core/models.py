f42b5e7748b39467be4a3616dc42de8760800b8a
Luiz-Lins <luizlins22@gmail.com>
2022-10-17 01:38:29 -0300

    Realizado migrate para utilização do banco de dados sql bem como a configuração dos campos em models.py, registrado em admin.py e criação de super usuário (admin).

--- desafio/core/models.py
+++ desafio/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    objects = None
+    nome = models.CharField('nome', max_length=250)
+    email = models.CharField('email', max_length=120)
+    data_nascimento = models.DateField('Data de nascimento')
+
+    def __str__(self):
+        return self.nome
