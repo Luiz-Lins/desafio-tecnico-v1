b2c1b39721a8230d367b54ed8f24b0c0889c6dd6
Luiz-Lins <luizlins22@gmail.com>
2022-10-21 20:56:50 -0300

    Alteração layout.

--- desafio/core/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from core.models import Pessoa
-
-
-class PessoaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pessoa
-        fields = ['id', 'nome', 'email', 'data_nascimento']
